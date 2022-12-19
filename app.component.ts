f5ab28a4b5d0b347c5d59f6365b58c2f9bd36599
LikaGreb <114307021+LikaGreb@users.noreply.github.com>
2022-12-19 16:46:32 +0200

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'my-app-lika';
+  text = "If you are new to Angular, you might want to start with Try it now!, which introduces the essentials of Angular in the context of a ready-made basic online store app for you to examine and modify. This standalone tutorial takes advantage of the interactive StackBlitz environment for online development. You don't need to set up your local environment until you're ready.";
+}
